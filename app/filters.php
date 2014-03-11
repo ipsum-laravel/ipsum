05f8333f227367301fdf7fb9a5587acdce262000
b1nj <postmaster@b1nj.fr>
2014-03-10 21:08:11 -0400

    fix login route

--- app/filters.php
+++ app/filters.php
@@ -35,7 +35,7 @@ App::after(function($request, $response)
 
 Route::filter('auth', function()
 {
-    if (Auth::guest()) return Redirect::guest('admin/login');
+    if (Auth::guest()) return Redirect::guest(route('admin.login'));
 
     // Check accès au controleur
     $controleur = strstr(Route::currentRouteAction(), '@', true);
