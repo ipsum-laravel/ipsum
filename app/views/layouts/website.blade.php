3590eaf5a0865830a5c3f5e1303d7b31daa88fff
b1nj <postmaster@b1nj.fr>
2014-07-27 16:18:33 -0400

    LAST COMMIT

--- app/views/layouts/website.blade.php
+++ app/views/layouts/website.blade.php
@@ -39,7 +39,7 @@
 
         <aside class="menu mod right w30 mls pam">
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                Lorem ipsum dolor sit amet, <strong>consectetur adipisicing</strong> elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
                 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
